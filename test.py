42fb8588286f2603d08eabeb6869ad3f22459470
dcpim <patrick@dendory.ca>
2023-12-11 20:38:45 +0000

    tests file

--- test.py
+++ test.py
@@ -2,3 +2,19 @@
 
 import dcpim
 import boto3
+
+# Mock session
+token = dcpim.guid()
+table = "dcpim.sessions.{}".format(token)
+dcpim.db_create(table)
+dcpim.db_put(table, "valid_until", "2038-01-01 12:00:00")
+dcpim.db_put(table, "from_ip", "127.0.0.1")
+dcpim.db_put(table, "username", "admin")
+dcpim.db_put(table, "roles", "|media.admin|")
+
+
+# Run tests
+
+
+# Delete session
+dcpim.db_delete(table)
