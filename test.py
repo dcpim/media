4305071c34007a955c3263f03bc264f6e6ebbd1f
Patrick Lambert <152328019+dcpim@users.noreply.github.com>
2023-12-11 15:57:08 -0500

    Update test.py

--- test.py
+++ test.py
@@ -4,17 +4,17 @@ import dcpim
 import boto3
 
 # Mock session
-mock_token = dcpim.guid()
-mock_table = "dcpim.sessions.{}".format(token)
-dcpim.db_create(mock_table)
-dcpim.db_put(mock_table, "valid_until", "2038-01-01 12:00:00")
-dcpim.db_put(mock_table, "from_ip", "127.0.0.1")
-dcpim.db_put(mock_table, "username", "admin")
-dcpim.db_put(mock_table, "roles", "|media.admin|")
+TOKEN = dcpim.guid()
+TABLE = "dcpim.sessions.{}".format(TOKEN)
+dcpim.db_create(TABLE)
+dcpim.db_put(TABLE, "valid_until", "2038-01-01 12:00:00")
+dcpim.db_put(TABLE, "from_ip", "127.0.0.1")
+dcpim.db_put(TABLE, "username", "admin")
+dcpim.db_put(TABLE, "roles", "|media.admin|")
 
 
 # Run tests
 
 
 # Delete session
-dcpim.db_delete(mock_table)
+dcpim.db_delete(TABLE)
